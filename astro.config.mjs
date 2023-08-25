1e8140bf309edb9906ef85f32b14d291732d78e0
Yao Ding <yaodingyd@gmail.com>
2023-08-25 10:02:08 -0400

    add prefetch and clean up style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yaodingyd.github.io/',
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind(), prefetch()]
 });
